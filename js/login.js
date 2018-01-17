5519c4d4e6d06a2f6846f3fdcd6312fd397f0557
margb13 <marg.b13@hotmail.com>
2018-01-17 16:24:12 -0500

    Añadiendo funcionalidad al boton de login gmail

--- js/login.js
+++ js/login.js
@@ -11,126 +11,43 @@ $(document).ready(function () {
   firebase.initializeApp(config);
 
   $(document).ready(function () {
-    var $loginGoogle = $('#google-login');
-    var $loginFb = $('#fb-login');
-    var $signOut = $('#sign-out');
+
     var $loginEmail = $('#email-login');
     var $email = $('#email');
     var $password = $('#password');
 
-    var $username = $('.displayUsername');
-    var $userEmail = $('#displayEmail');
-    var $profilePhoto = $('#profile-photo');
+    var $loginGoogle = $('#google-login');
 
-    // Login con email
-    $loginEmail.click(function (event) {
-      event.preventDefault();
-
-      var email = $email.val();
-      var password = $password.val();
-
-      firebase.auth().signInWithEmailAndPassword(email, password)
-        .catch(function (error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // ...
-          $('#login-help').removeClass('d-none');
-        });
-
-      firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          $(location).attr('href', 'home.html');
-        }
-      });
-    });
 
     // Login con Google
     var providerGoogle = new firebase.auth.GoogleAuthProvider();
+
     $loginGoogle.click(function () {
       firebase.auth().signInWithPopup(providerGoogle).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        firebase.database().ref('users/' + user.uid).set({
-          name: user.displayName,
-          email: user.email,
-          uid: user.uid,
-          profilePhoto: user.photoURL
-        }).then(
-          user => {
-            $(location).attr('href', 'home.html');
-          });
-        // ...
-      }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+        console.log(result.user);
+        window.localStorage.setItem('storageUID', result.user.uid);
+        // Guardando el UID en el localstorage
+        var UID = window.localStorage.getItem('storageUID');
+        console.log(UID);
+
+        // Redireccionando al perfil
+        alert('Registro exitoso');
       });
     });
 
-    // Login con Facebook
-    var providerFb = new firebase.auth.FacebookAuthProvider();
-    $loginFb.click(function () {
-      firebase.auth().signInWithPopup(providerFb).then(function (result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        firebase.database().ref('users/' + user.uid).set({
-          name: user.displayName,
-          email: user.email,
-          profilePhoto: user.photoURL,
-        }).then(user => {
-          window.location.href = 'home.html';
-        });
-        // ...
-      }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-    });
 
-    // Obteniendo datos del usuario actual
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        // User is signed in.
-        var name = user.displayName;
-        var email = user.email;
-        var photoUrl = user.photoURL;
-        var emailVerified = user.emailVerified;
-        var uid = user.uid;
-        // console.log(user);
-        $username.text(name);
-        $userEmail.text(email);
-        $profilePhoto.attr('src', photoUrl);
-      } else {
-        // No user is signed in.
-      }
-    });
+    function saveData(user) {
+      console.log(user);
+      var userToSave = {
+        uid: user.uid,
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL
+      };
+      // probando es el nombre de tu rama
+  
+      firebase.database().ref('users/' + user.uid).set(userToSave); // push añade un registro 
+    }
 
-    // Cerrar sesión
-    $signOut.click(function () {
-      firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-        console.log('Cerrando sesión...');
-        $(location).attr('href', 'login.html');
-      }).catch(function (error) {
-        // An error happened.
-      });
-    });
   });
-
 });
